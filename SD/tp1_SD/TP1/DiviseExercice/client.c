99885385488b6d2b97385a094647361325bb7666
Eneru <jeremy.meyer@etu.unistra.fr>
2014-02-14 10:12:34 +0100

    Debut tp1 sd

---
+++ SD/tp1_SD/TP1/DiviseExercice/client.c
@@ -0,0 +1,18 @@
+#include "include.h"
+
+int main (int argc, char **argv) {
+  char *host = argv[1];
+  enum clnt_stat stat;
+  entiers2 res;
+  entiers2 donnees = {13 , 5};
+  stat = callrpc(???); 
+
+  if (stat != RPC_SUCCESS) { 
+    fprintf(stderr, "Echec de l'appel distant\n");
+    clnt_perrno(stat);      fprintf(stderr, "\n");
+  } else {
+    printf("client res : %d/%d (q:%d r:%d)\n",
+	   donnees.x,donnees.y,res.x,res.y);
+  }
+  return(0);
+}
---
+++ SD/tp1_SD/TP1/DiviseExercice/serveur.c
@@ -0,0 +1,17 @@
+#include "include.h"
+
+entiers2 * divise(entiers2 *e) { 
+  ????
+}
+int main (void) {
+  bool_t stat;
+  stat = registerrpc(????);
+  if (stat != 0) {
+    fprintf(stderr,"Echec de l'enregistrement\n");
+    exit(1);
+  }
+  svc_run(); /* le serveur est en attente de clients eventuels */
+  return(0); /* on y passe jamais ! */
+}
+
+
