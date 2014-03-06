9a756a9ef1c387d625fdcaeb68d4bf644e8cda00
Eneru <jeremy.meyer@etu.unistra.fr>
2014-03-06 15:39:34 +0100

    debut tp4 ihm

---
+++ IHM/TP4/main.cpp
@@ -0,0 +1,63 @@
+#include "mainframe.h"
+
+class MyApp: public wxApp 
+{
+	virtual bool OnInit();
+	
+	//Attribut
+	CMainFrame *m_MainFrame;
+};
+
+
+IMPLEMENT_APP(MyApp)
+
+
+bool MyApp::OnInit() 
+{
+	//Fenetre principale
+	m_MainFrame = new CMainFrame( wxString("Fenetre", wxConvUTF8), wxPoint(50,50), wxSize(450,340) );
+	//Barre de menus
+	m_MainFrame->menu_bar = new wxMenuBar;
+	//Menu
+	m_MainFrame->file_menu = new wxMenu;
+	m_MainFrame->display_menu = new wxMenu;
+	m_MainFrame->options_menu = new wxMenu;
+	m_MainFrame->help_menu = new wxMenu;
+	
+	//Ajout d'élément dans la barre de menus
+	m_MainFrame->menu_bar->Append(m_MainFrame->file_menu, wxT("Fichier"));
+	m_MainFrame->menu_bar->Append(m_MainFrame->display_menu, wxT("Affichage"));
+	m_MainFrame->menu_bar->Append(m_MainFrame->options_menu, wxT("Options"));
+	m_MainFrame->menu_bar->Append(m_MainFrame->help_menu, wxT("Aide"));
+	
+	//Menu de fichiers
+	m_MainFrame->file_menu->Append(MENU_NEW, wxT("Nouveau\tCtrl-N"));
+	m_MainFrame->file_menu->AppendSeparator();
+	m_MainFrame->file_menu->Append(MENU_OPEN, wxT("Ouvrir\tCtrl-O"));
+	m_MainFrame->file_menu->Append(MENU_SAVE, wxT("Sauvegarder\tCtrl-S"));
+	m_MainFrame->file_menu->AppendSeparator();
+	m_MainFrame->file_menu->Append(MENU_QUIT, wxT("Quitter\tCtrl-X"));
+	
+	//Menu d'options
+	m_MainFrame->options_menu->Append(MENU_THICKNESS, wxT("Epaisseur de trait"));
+	m_MainFrame->options_menu->Append(MENU_COLOR, wxT("Couleur"));
+	m_MainFrame->options_menu->Append(MENU_TRIANGLE_MANAGEMENT, wxT("Gestion des triangles"));
+	m_MainFrame->options_menu->Enable(MENU_TRIANGLE_MANAGEMENT, false);
+
+	//Menu d'aide
+	m_MainFrame->help_menu->Append(MENU_VERSION, wxT("Version"));
+	
+	//Menu d'affichage
+	m_MainFrame->display_menu->AppendCheckItem(MENU_TOOLBAR, wxT("Barre d'outils"));
+	m_MainFrame->display_menu->Check(MENU_TOOLBAR,true);
+
+	//Affichage
+	m_MainFrame->SetMenuBar(m_MainFrame->menu_bar);
+	m_MainFrame->CreateMyToolbar();
+	m_MainFrame->Show(TRUE);
+	
+	
+	return TRUE;
+} 
+
+
